2b50f155075e3caa7647f033b3a127568098a5ae
faine1996 <faine@Faine-Omen.>
2025-04-01 20:04:09 +0300

    Add Cell class with wall-drawing logic and integrate with Window

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from graphics import Window
 from shapes import Line,Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-     # Create a few points and lines
-    p1 = Point(100, 100)
-    p2 = Point(300, 100)
-    p3 = Point(300, 300)
-    p4 = Point(100, 300)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
 
-    line1 = Line(p1, p2)
-    line2 = Line(p2, p3)
-    line3 = Line(p3, p4)
-    line4 = Line(p4, p1)
-
-    # Draw lines on the window
-    win.draw_line(line1, "red")
-    win.draw_line(line2, "green")
-    win.draw_line(line3, "blue")
-    win.draw_line(line4, "purple")
 
     win.wait_for_close()
 
